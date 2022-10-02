a6723031a4cac904e587aa3bb13c43629b490564
Anelliya <najdenova.elena@gmail.com>
2022-10-02 22:18:03 +0300

    Skip status validation on update if no status provided

--- src/notes/dto/update-note.dto.ts
+++ src/notes/dto/update-note.dto.ts
@@ -1,10 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateNoteDto } from './create-note.dto';
-import { IsIn } from 'class-validator';
+import { IsIn, ValidateIf } from 'class-validator';
 
 import { Status as NoteStatus } from '../entities/note.entity';
 
 export class UpdateNoteDto extends PartialType(CreateNoteDto) {
   @IsIn(Object.values(NoteStatus))
+  @ValidateIf((object, value) => value !== undefined)
   status: string;
 }
